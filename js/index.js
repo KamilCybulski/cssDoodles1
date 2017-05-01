445a0d7736ac0accf477b996dbe771be7b31fef4
Kamyl <kamillcybulski@gmail.com>
2017-05-01 23:12:04 +0200

    Added nav-scroller + basic microdata

--- js/index.js
+++ js/index.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
 
     //parallax initialization
     $('.parallax').parallax();
+
+    //smooth scrolling for navbar links
+    $(".nav-wrapper li a").click(function(){
+        var adress = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(adress).offset().top});
+    })
+
+    //As above for mobile side-nav
+    $(".side-nav li a").click(function(){
+        var adress = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(adress).offset().top});
+    })
 });
